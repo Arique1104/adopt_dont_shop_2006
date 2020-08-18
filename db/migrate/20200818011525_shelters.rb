a51623c2f2a524b3f3206d97f3b1c08fe56d5545
Arique <arique.aguilar@gmail.com>
2020-08-18 11:40:28 -0500

    Changed method in Active Record

--- db/migrate/20200818011525_shelters.rb
+++ db/migrate/20200818011525_shelters.rb
@@ -1,11 +1,11 @@
 class Shelters < ActiveRecord::Migration[5.2]
   def change
-    create_table :shelters do |s|
-      s.string :name
-      s.string :address
-      s.string :city
-      s.string :state
-      s.string :zip
+    create_table :shelters do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
     end
   end
 end
